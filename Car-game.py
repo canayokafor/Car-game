2c649bc16d6533b1a589395e6a37f1c1d346de57
Chinedu Anayokafor <38936447+nedu01@users.noreply.github.com>
2019-04-09 14:38:36 +0000

    file created

---
+++ Car-game.py
@@ -0,0 +1,26 @@
+# A car game engine
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car is already started")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped...")
+        else:
+            started = False
+            print("Card stopped..")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to exit""")
+    elif command == "quit":
+        break
+    else:
+        print("I don't know what you typed")
